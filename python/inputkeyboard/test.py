846757796cc82d9375bf1cd50a0304774c59ad15
gotankgo <goodbadtank@gmail.com>
2017-02-01 17:28:42 +0900

    Update test.py

--- python/inputkeyboard/test.py
+++ python/inputkeyboard/test.py
@@ -1,7 +1,11 @@
 import Inputkey
 
+key = Inputkey.get()
+if 27 == ord(key):
+    exit()
+
 while True:
-    key = Inputkey.get()
+    key = Inputkey.arrow_get()
     if key == 'esc':
         exit()
     else:
