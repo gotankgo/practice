9a2abf0af08efda324f2b91eb314a97e3f6e78a7
gotankgo <goodbadtank@gmail.com>
2017-02-14 11:22:48 +0900

    Create test.py

---
+++ python/Syntax/test.py
@@ -0,0 +1,14 @@
+a ? b : c 
+b if a else c 
+a 조건식이라는 가정 ㅇㅇ (예를들어 a -> name == "Michael")
+
+
+[result for result in resultSets if result == True]
+
+matrix[100:300, 100:300]
+
+
+파이썬 코루틴
+ 제네레이터
+ 피클링?
+ yield
